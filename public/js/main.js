c30419a53f5ca4eb79124b0d2cd3618c9c6918a3
Drakunal <drakunaldey1@gmail.com>
2021-03-13 21:47:30 +0530

    send message to console of client:

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
+const chartForm = document.getElementById('chat-form');
+
+
 const socket = io();
 
 
 socket.on('message',message=>{
     console.log(message);
 });
+
+//messages
+chartForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+    console.log(msg);
+
+})
